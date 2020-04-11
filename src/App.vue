ae97bf9affb9c51b9f81a69a85552bd4364af253
ZHUZHENQUAN <zhuzhenquan@linkingvision.com>
2020-04-11 17:32:45 +0800

    app登录页面和字体修改和底部导航固定

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-     <!-- <router-view/> -->
+  <div id="app"> 
+     <router-view ></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+   
+  },
+
+      
 }
 </script>
 
 <style>
+html,body{
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #252527;
+}
+
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+  /* margin-top: 60px; */
+ }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,47 @@
 import Vue from 'vue'
-import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import App from './App'
+import store from './store'
+// import "@/amfe-flexible/index.js";
+// import axios from '@/http'
 
-
+// const routerPush = router.prototype.push
+// router.prototype.push = function push(location) {
+//   return routerPush.call(this, location).catch(error=> error)
+// }
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
+   // designSize:设计图尺寸(传@1x倍图尺寸,例如设计图是750px,2倍图,那么传375),rootValue:比例,默认100
+   function sethtml(designSize, rootValue){
+    let rootV = 100;
+    if (rootValue) {
+        rootV = rootValue;
+    }
+    // 窗口宽度
+    const winWidth = window.document.documentElement.clientWidth;
+    // 如果窗口宽度超过1.5倍设计图了,则不再调整
+    if (winWidth > 1.5 * designSize) { return; }
+    // window.console.log(winWidth);
+    const htmlFontSize = winWidth * rootV / designSize;
+    window.document.documentElement.style.fontSize = htmlFontSize + 'px';
+} sethtml(375)
+// http request 拦截器
+axios.interceptors.request.use(
+  config => {
+    // console.log('http request');
+    if (store.state.token) {
+      config.headers.Authorization = `token ${store.state.token}`
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+ render: h => h(App),
 }).$mount('#app')
